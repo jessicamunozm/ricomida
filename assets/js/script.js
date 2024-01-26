c77cea6b8a0fe3029f31b498df86f0ecc7bd26e7
Jessica Munoz <jessica.munozm@mail.udp.cl>
2024-01-25 23:37:46 -0300

    finalizado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,31 @@
+$(document).ready(function() {
+
 // ENABLE TOOLTIPS DE BOOTSTRAP.COM
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
 tooltipTriggerList.forEach(tooltipTriggerEl => {
     new bootstrap.Tooltip(tooltipTriggerEl);
 });
+
+
+$("#ingredientes").dblclick(function () { 
+    $(this).css("color","red") 
+    
+})
+
+$("#preparacion").dblclick(function () { 
+    $(this).css("color","red") 
+    
+})
+
+// NOTA: NO ENCONTRÉ CÓMO EDITAR LA VENTANA ALERT
+$(document).ready(function () {
+$("#button1").click(function () { 
+alert('El correo ha sido enviado correctamente');
+});
+})
+
+$(".card-text").click(function () { 
+$(".cardtext").hide();
+    })
+ })
+  
